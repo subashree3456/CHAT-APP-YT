674628dd7557792744e43b3e6916f0dd4d888bf0
SUBASHREE SANKARANARAYANAN <subashree3003@gmail.com>
2024-06-26 16:41:47 +0530

    commit-6

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
-
+//const __dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json());
@@ -24,10 +24,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(( "./frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join( "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, ()=> {
